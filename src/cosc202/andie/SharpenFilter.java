090f41339549906fb4b49865dcb3f0e71033c9a1
KatHogg05 <kathogg05@users.noreply.github.com>
2024-03-19 14:10:06 +1300

    Pushing alterted FilterActions file and other sharpen edits

--- src/cosc202/andie/SharpenFilter.java
+++ src/cosc202/andie/SharpenFilter.java
@@ -1,6 +1,7 @@
 package cosc202.andie;
 
 import java.awt.image.*;
+import java.util.Arrays;
 
 /**
  * Katrina's job >:3
@@ -12,8 +13,22 @@ public class SharpenFilter implements ImageOperation, java.io.Serializable{
     }
 
     public BufferedImage apply(BufferedImage input) {
-        
+        // The values for the kernel as a 9-element array
+        float[] array = { 0, -1 / 2.0f, 0,
+                        -1 / 2.0f, 3, -1 / 2.0f,
+                        0, -1 / 2.0f, 0 };
 
-        return input;
+        // Make a 3x3 filter from the array
+        Kernel kernel = new Kernel(3, 3, array);
+        // Apply this as a convolution - same code as in MeanFilter
+        ConvolveOp convOp = new ConvolveOp(kernel);
+
+        BufferedImage output = new BufferedImage(input.getColorModel(),
+                input.copyData(null),
+                input.isAlphaPremultiplied(), null);
+        convOp.filter(input, output);
+        // And we're done
+        return output;
+       
     }
 }
