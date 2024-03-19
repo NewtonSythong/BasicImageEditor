1ea8f15f18801290ed07d3485b774d05a8293780
KatHogg05 <kathogg05@users.noreply.github.com>
2024-03-19 14:07:02 +1300

    Implementing sharpen filter

---
+++ src/cosc202/andie/SharpenFilter.java
@@ -0,0 +1,19 @@
+package cosc202.andie;
+
+import java.awt.image.*;
+
+/**
+ * Katrina's job >:3
+ */
+public class SharpenFilter implements ImageOperation, java.io.Serializable{
+    
+    SharpenFilter(){
+        //construction Code
+    }
+
+    public BufferedImage apply(BufferedImage input) {
+        
+
+        return input;
+    }
+}
