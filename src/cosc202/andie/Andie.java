0aae8f1afe7a8b14172863ad7f9aac925c991f63
sytne506 <sytne506@student.otago.ac.nz>
2024-03-28 00:00:51 +1300

    Remove unused language-related classes and attempt at fixing multi language feature

--- src/cosc202/andie/Andie.java
+++ src/cosc202/andie/Andie.java
@@ -2,18 +2,18 @@ package cosc202.andie;
 
 import java.awt.BorderLayout;
 import java.awt.Image;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JScrollPane;
-import java.util.Locale;
 
 /**
  * <p>
  * Main class for A Non-Destructive Image Editor (ANDIE).
-* ANDIE BETTER WORK FOR TEAM X
+ * ANDIE BETTER WORK FOR TEAM X
  * CAN EVERYONE SEE THIS COMMENT?!
  * 
  * Does my comment work on this file
@@ -24,17 +24,20 @@ import java.util.Locale;
  * 
  * <p>
  * This class is the entry point for the program.
- * It creates a Graphical User Interface (GUI) that provides access to various image editing and processing operations.
+ * It creates a Graphical User Interface (GUI) that provides access to various
+ * image editing and processing operations.
  * </p>
  * 
  * <p>
- * <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a>
+ * <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA
+ * 4.0</a>
  * </p>
  * 
  * @author Steven Mills
  * @version 1.0
  */
 public class Andie {
+    public static JFrame frame;
 
     /**
      * <p>
@@ -42,8 +45,10 @@ public class Andie {
      * </p>
      * 
      * <p>
-     * This method sets up an interface consisting of an active image (an {@code ImagePanel})
-     * and various menus which can be used to trigger operations to load, save, edit, etc. 
+     * This method sets up an interface consisting of an active image (an
+     * {@code ImagePanel})
+     * and various menus which can be used to trigger operations to load, save,
+     * edit, etc.
      * These operations are implemented {@link ImageOperation}s and triggered via
      * {@code ImageAction}s grouped by their general purpose into menus.
      * </p>
@@ -59,14 +64,17 @@ public class Andie {
      * 
      * @throws Exception if something goes wrong.
      */
-    private static void createAndShowGUI() throws Exception {
-        // Set up the main GUI frame
-        JFrame frame = new JFrame("ANDIE");
-
+    public static void createAndShowGUI() throws Exception {
+        if (frame == null) {
+            frame = new JFrame("ANDIE");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        } else {
+            frame.getContentPane().removeAll();
+        }
+        
         Locale.setDefault(new Locale("en", "NZ"));
 
         ResourceBundle bundle = ResourceBundle.getBundle("cosc202/andie/MessageBundle", Locale.getDefault());
-
 
         Image image = ImageIO.read(Andie.class.getClassLoader().getResource("icon.png"));
         frame.setIconImage(image);
@@ -77,11 +85,12 @@ public class Andie {
         ImageAction.setTarget(imagePanel);
         JScrollPane scrollPane = new JScrollPane(imagePanel);
         frame.add(scrollPane, BorderLayout.CENTER);
-        
+
         // Add in menus for various types of action the user may perform.
         JMenuBar menuBar = new JMenuBar();
 
-        // File menus are pretty standard, so things that usually go in File menus go here.
+        // File menus are pretty standard, so things that usually go in File menus go
+        // here.
         FileActions fileActions = new FileActions(bundle);
         menuBar.add(fileActions.createMenu());
 
@@ -89,28 +98,34 @@ public class Andie {
         EditActions editActions = new EditActions(bundle);
         menuBar.add(editActions.createMenu());
 
-        // View actions control how the image is displayed, but do not alter its actual content
+        // View actions control how the image is displayed, but do not alter its actual
+        // content
         ViewActions viewActions = new ViewActions(bundle);
         menuBar.add(viewActions.createMenu());
 
-        // Filters apply a per-pixel operation to the image, generally based on a local window
+        // Filters apply a per-pixel operation to the image, generally based on a local
+        // window
         FilterActions filterActions = new FilterActions(bundle);
         menuBar.add(filterActions.createMenu());
 
         // Actions that affect the representation of colour in the image
         ColourActions colourActions = new ColourActions(bundle);
         menuBar.add(colourActions.createMenu());
-        
+
         ImageActions imageActions = new ImageActions(bundle);
         menuBar.add(imageActions.createMenu());
 
         // Language menu
-        LanguageActions2 LanguageActions2 = new LanguageActions2(bundle);
+        LanguageActions2 LanguageActions2 = new LanguageActions2(bundle, frame);
         menuBar.add(LanguageActions2.createMenu());
 
         frame.setJMenuBar(menuBar);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public static JFrame getFrame() { // Add this method
+        return frame;
     }
 
     /**
@@ -139,4 +154,7 @@ public class Andie {
             }
         });
     }
+
+
+
 }
