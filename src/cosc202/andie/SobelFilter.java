58692bdaac326810ce9efa7ea234c4081cba2f13
Newton <sytne506@student.otago.ac.nz>
2024-04-30 23:02:34 +1200

    nearly done

---
+++ src/cosc202/andie/SobelFilter.java
@@ -0,0 +1,47 @@
+package cosc202.andie;
+
+import java.awt.image.BufferedImage;
+
+import javax.swing.JOptionPane;
+
+public class SobelFilter implements ImageOperation {
+
+    int direction;
+
+    public SobelFilter() {
+        this.direction = 1;
+    }
+
+    public SobelFilter(int direction) {
+        this.direction = direction;
+    }
+
+    public BufferedImage apply(BufferedImage input) {
+        try {
+
+            if (input != null) {
+                float[][] kernels = {
+                        {
+                                -(1 / 2f), 0, 1 / 2,
+                                -1, 0, 1,
+                                -(1 / 2), 0, 1 / 2
+                        },
+                        {
+                                -(1 / 2), -1, -(1 / 2),
+                                0, 0, 0,
+                                1 / 2, 1, 1 / 2
+                        }
+                };
+
+                BufferedImage output = ConvolutionNew.apply(input, kernels[direction], 1);
+                return output;
+            } else
+                throw new NullPointerException();
+        } catch (NullPointerException e) {
+            JOptionPane.showMessageDialog(null, "Please select an image before filter");
+            return null;
+
+        }
+
+    }
+}
