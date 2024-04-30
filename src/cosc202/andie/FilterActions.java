7a8da71342ba05dc05135a21c5f7b0b318e01e75
Newton <sytne506@student.otago.ac.nz>
2024-04-30 16:24:15 +1200

    GOT BLOCK AVERAGE TO FINALLY FUNCTION

--- src/cosc202/andie/FilterActions.java
+++ src/cosc202/andie/FilterActions.java
@@ -71,6 +71,8 @@ public class FilterActions {
                 Integer.valueOf(KeyEvent.VK_G)));
         actions.add(new SharpenFilterAction(bundle.getString("SharpenFilter"), null, "Apply a sharpen filter",
                 Integer.valueOf(KeyEvent.VK_S)));
+        actions.add(new BlockAveragingAction(bundle.getString("BlockAverage"), null, "Apply a block average",
+                Integer.valueOf(KeyEvent.VK_A)));
 
     }
 
@@ -442,4 +444,90 @@ public class FilterActions {
             target.getParent().revalidate();
         }
     }
+
+
+    /**
+     * 
+     * <p>
+     * 
+     * Action to blur an image with a block averaging filter.
+     * 
+     * </p>
+     *
+     * 
+     * 
+     * @see BlockAveraging
+     * 
+     */
+    public class BlockAveragingAction extends ImageAction {
+        /**
+         * 
+         * <p>
+         * 
+         * Create a new block-averaging-filter action.
+         * 
+         * </p>
+         *
+         * 
+         * 
+         * @param name     The name of the action (ignored if null).
+         * 
+         * @param icon     An icon to use to represent the action (ignored if null).
+         * 
+         * @param desc     A brief description of the action (ignored if null).
+         * 
+         * @param mnemonic A mnemonic key to use as a shortcut (ignored if null).
+         * 
+         */
+        BlockAveragingAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
+
+            super(name, icon, desc, mnemonic);
+
+        }
+
+        public void actionPerformed(ActionEvent e) {
+
+            // create and apply filter
+
+            // determine block size - ask user.
+
+            int blockWidth = 1;
+            int blockHeight = 1;
+
+            // Pop-up dialog box to ask for block size value.
+
+            SpinnerNumberModel blockWidthModel = new SpinnerNumberModel(1, 1, 10, 1);
+            SpinnerNumberModel blockHeightModel = new SpinnerNumberModel(1, 1, 10, 1);
+
+            JSpinner blockWidthSpinner = new JSpinner(blockWidthModel);
+            JSpinner blockHeightSpinner = new JSpinner(blockHeightModel);
+
+            int option = JOptionPane.showOptionDialog(null, blockWidthSpinner, "Enter block width",
+                    JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+
+            // Check the return value from the dialog box.
+
+            if (option == JOptionPane.CANCEL_OPTION) {
+                return;
+            } else if (option == JOptionPane.OK_OPTION) {
+                blockWidth = blockWidthModel.getNumber().intValue();
+            }
+
+            option = JOptionPane.showOptionDialog(null, blockHeightSpinner, "Enter block height",
+                    JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+
+            // Check the return value from the dialog box.
+
+            if (option == JOptionPane.CANCEL_OPTION) {
+                return;
+            } else if (option == JOptionPane.OK_OPTION) {
+                blockHeight = blockHeightModel.getNumber().intValue();
+            }
+
+            target.getImage().apply(new BlockAveraging(blockWidth, blockHeight));
+            target.repaint();
+            target.getParent().revalidate();
+        }
+    }
+
 }
