d005a4d6e72b0a574545de1eff5fca5fc037c224
Newton <sytne506@student.otago.ac.nz>
2024-05-14 14:53:07 +1200

    Added javadoc comments

--- src/cosc202/andie/KeyboardShortcuts.java
+++ src/cosc202/andie/KeyboardShortcuts.java
@@ -1,66 +1,86 @@
 package cosc202.andie;
 
-import javax.swing.Action;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
+import javax.swing.Action;
+
+/**
+ * This class represents the keyboard shortcuts for an application.
+ * It manages a list of actions that can be triggered by keyboard events.
+ * 
+ * @author Cam Clark
+ */
 public class KeyboardShortcuts {
 
+    /**
+     * The KeyAdapter object that handles keyboard events.
+     */
     KeyAdapter keyAdapter1;
 
+    /**
+     * Constructs a new KeyboardShortcuts object.
+     * It initializes the KeyAdapter and sets up the keyboard shortcuts.
+     */
     public KeyboardShortcuts() {
 
         ArrayList<Action> fileActions = new FileActions().actions;
         ArrayList<Action> editActions = new EditActions().actions;
         ArrayList<Action> imageActions = new ImageActions().actions;
         keyAdapter1 = new KeyAdapter() {
+            /**
+             * Handles the keyPressed event.
+             * It triggers the corresponding action based on the key pressed.
+             * 
+             * @param e the keyboard event.
+             */
             @Override
             public void keyPressed(KeyEvent e) {
                 int key = e.getKeyCode();
                 switch (key) {
                     // File actions shortcuts
                     case KeyEvent.VK_O:
-                    fileActions.get(0).actionPerformed(null);
+                        fileActions.get(0).actionPerformed(null);
                         break;
                     case KeyEvent.VK_S:
-                    fileActions.get(1).actionPerformed(null);
+                        fileActions.get(1).actionPerformed(null);
                         break;
                     case KeyEvent.VK_A:
-                    fileActions.get(2).actionPerformed(null);
+                        fileActions.get(2).actionPerformed(null);
                         break;
                     case KeyEvent.VK_E:
-                    fileActions.get(3).actionPerformed(null);
+                        fileActions.get(3).actionPerformed(null);
                         break;
                     case KeyEvent.VK_0:
-                    fileActions.get(4).actionPerformed(null);
+                        fileActions.get(4).actionPerformed(null);
                         break;
                     // edit actions shortcuts
                     case KeyEvent.VK_Z:
-                    editActions.get(0).actionPerformed(null);
+                        editActions.get(0).actionPerformed(null);
                         break;
                     case KeyEvent.VK_Y:
-                    editActions.get(1).actionPerformed(null);
+                        editActions.get(1).actionPerformed(null);
                         break;
                     // image actions shortcuts
-                    case KeyEvent.VK_C : 
-                    imageActions.get(0).actionPerformed(null);
-                    break;
-                    case KeyEvent.VK_Q : 
-                    imageActions.get(1).actionPerformed(null);
-                    break;
-                    case KeyEvent.VK_H : 
-                    imageActions.get(2).actionPerformed(null);
-                    break;
-                    case KeyEvent.VK_G : 
-                    imageActions.get(3).actionPerformed(null);
-                    break;
-                    case KeyEvent.VK_B : 
-                    imageActions.get(4).actionPerformed(null);
-                    break;
-                    case KeyEvent.VK_L : 
-                    imageActions.get(5).actionPerformed(null);
-                    break;
+                    case KeyEvent.VK_C:
+                        imageActions.get(0).actionPerformed(null);
+                        break;
+                    case KeyEvent.VK_Q:
+                        imageActions.get(1).actionPerformed(null);
+                        break;
+                    case KeyEvent.VK_H:
+                        imageActions.get(2).actionPerformed(null);
+                        break;
+                    case KeyEvent.VK_G:
+                        imageActions.get(3).actionPerformed(null);
+                        break;
+                    case KeyEvent.VK_B:
+                        imageActions.get(4).actionPerformed(null);
+                        break;
+                    case KeyEvent.VK_L:
+                        imageActions.get(5).actionPerformed(null);
+                        break;
                 }
             }
         };
